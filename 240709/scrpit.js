be2ec4fe7c945057c288e1d00b3e7628a73cf315
dghasdf <az3014@naver.com>
2024-07-09 18:33:52 +0900

    240709

---
+++ 240709/scrpit.js
@@ -0,0 +1,8 @@
+const circle = document.querySelector("#circle");
+const article = circle.querySelector("article");
+
+article.forEach((el) => {
+  el.addEVverntlistener("mouseentr", () => {
+  circle.
+})
+})
--- 프로젝트/script.js
+++ 프로젝트/script.js
@@ -1,3 +1,24 @@
+const gnbLi = document.querySelectorAll(".lnb > li");
+gnbLi.forEach((li) => {
+  li.addEventListener("mouseover", () => {
+    const lnb = li.querySelector(".lnb");
+    const aTag = li.querySelector("a");
+    if (lnb) {
+      lnb.style.maxHeight = lnb.scrollHeight + "px";
+      lnb.style.opacity = "1";
+      aTag.classList.add("active");
+    }
+  });
+  li.addEventListener("mouseout", () => {
+    const lnb = li.querySelector(".lnb");
+    const aTag = li.querySelector("a");
+    if (lnb) {
+      lnb.style.maxHeight = "0";
+      lnb.style.opacity = "0";
+      aTag.classList.remove("active");
+    }
+  });
+});
 /* 글씨 타이핑 효과 */
 const $txt = document.querySelector(".about_text");
 const content = "방갑습니다!\n노력의 대명사\n김준영입니다 :)";
