13e5804d23f3b8cf9f6a9eaa50a0658ec6148511
dghasdf <az3014@naver.com>
2024-07-17 19:11:59 +0900

    240717

--- 240715/flex2.js
+++ 240715/flex2.js
@@ -1,7 +1,6 @@
-// 스크롤을 내렸을 때의 이벤트
+// Header Nav
 window.addEventListener("scroll", () => {
   let scroll = window.scrollY;
-  console.log(scroll);
   const header = document.querySelector("header");
 
   if (scroll > 50) {
@@ -9,4 +8,41 @@ window.addEventListener("scroll", () => {
   } else {
     header.classList.remove("active");
   }
+});
+
+// Slick Slider
+$(".myslider").slick({
+  dots: false,
+  infinite: true,
+  autoplay: true,
+  speed: 1000,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        infinite: true,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+      },
+    },
+  ],
 });
