743850d159cf31e34ba1b945b615082d181b4461
dghasdf <az3014@naver.com>
2024-07-29 19:00:41 +0900

    240729

---
+++ 240729/01js/script.js
@@ -0,0 +1,54 @@
+// const test1 = "mon";
+//test1라는 변수에 변수명을 선언한다 이름은mon이다. 즉 변수명은 mon
+
+// test1 = "tue";
+// const test1 = "tue"; =  개발자 도구에러: SyntaxError: Identifier 'test1' has already been declared = 동일 변수에 대한 에러(재선언)
+
+//test1 = "tue"; = TypeError: Assignment to constant variable. = 이미 변수가 넣어져잇다. (에러)
+
+//프로그램 개발
+// -width 1920px 설정
+// 슬라이드 개수5개며 고정이면 const
+
+// 스크롤을 내리게 되었을 대, 내려간 값에 따라 무언가 변해야한다? = let
+
+// let test2 = "mon";
+
+// test2 = "tue";
+
+// const x = 10;
+// const y = 20;
+
+// const z = x + y;
+// console.log(z);
+
+// var x = 10;
+
+// var z = x + y;
+
+// var y = 20;
+// console.log(z);
+//=NaN
+
+// const num1 = 1;
+// const num2 = 2;
+
+// const sum = num1 + num2;
+// console.log(typeof sum);
+
+// const str1 = "1";
+// const str2 = "2";
+
+// console.log(typeof str1);
+
+// const num = 1000;
+// const str = "원";
+
+// const counter = num + str;
+// console.log(counter);
+
+const num1 = 1000;
+const str1 = "2000";
+
+const count1 = num1 + str1;
+console.log(count1);
