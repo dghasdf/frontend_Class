ab4e25b13fe7d6e0b46daf4b215159c1fdaf1986
dghasdf <az3014@naver.com>
2024-08-08 21:52:38 +0900

    240808

--- 240806/script.js
+++ 240806/script.js
@@ -1,25 +1,58 @@
 // window.addEventListener("load", alert("즐거운 화요일")); //load
 
-const overout = document.querySelectorAll(".overout");
-let i = 0;
+// const overout = document.querySelectorAll(".overout");
+// let i = 0;
 
-overout.forEach((item, index) => {
-  if (index === 0) {
-    item.addEventListener("mouseover", function () {
-      this.querySelector("p:first-of-type").innerText = "mouseover";
-      this.querySelector("p:last-of-type").innerText = ++i;
-    });
-    item.addEventListener("mouseout", function () {
-      this.querySelector("p:first-of-type").innerText = "mouseout";
-    });
-  }
-  if (index === 1) {
-    item.addEventListener("mouseover", function () {
-      this.querySelector("p:first-of-type").innerText = "mouseover";
-      this.querySelector("p:last-of-type").innerText = ++i;
-    });
-    item.addEventListener("mouseout", function () {
-      this.querySelector("p:first-of-type").innerText = "mouseout";
-    });
-  }
+// overout.forEach((item, index) => {
+//   if (index === 0) {
+//     item.addEventListener("mouseover", function () {
+//       this.querySelector("p:first-of-type").innerText = "mouseover";
+//       this.querySelector("p:last-of-type").innerText = ++i;
+//     });
+//     item.addEventListener("mouseout", function () {
+//       this.querySelector("p:first-of-type").innerText = "mouseout";
+//     });
+//   }
+//   if (index === 1) {
+//     item.addEventListener("mouseover", function () {
+//       this.querySelector("p:first-of-type").innerText = "mouseover";
+//       this.querySelector("p:last-of-type").innerText = ++i;
+//     });
+//     item.addEventListener("mouseout", function () {
+//       this.querySelector("p:first-of-type").innerText = "mouseout";
+//     });
+//   }
+// });
+
+let = o = 0;
+
+const overoutOut = document.querySelector("div.over.overout");
+overoutOut.addEventListener("mouseover", function () {
+  const pItems = this.querySelector("p");
+  pItems[0].innerText = "mouseover";
+  pItems[3].innerText = ++o;
+});
+
+const overoutIn = document.querySelector("div.in.overout");
+overoutIn.addEventListener("mouseover", function () {
+  const pItems = this.querySelectorAll("p");
+  pItems[0].innerText = "mouseover";
+  pItems[1].innerText = ++o;
+});
+
+//mouseenter & mouseleave
+let e = 0;
+
+const overleaveOut = document.querySelector("div.over.enterleave");
+overoutOut.addEventListener("mouseenter", function () {
+  const pItems = this.querySelector("p");
+  pItems[0].innerText = "mouseenter";
+  pItems[3].innerText = ++e;
+});
+
+const overleaveIn = document.querySelector("div.in.enterleave");
+overoutIn.addEventListener("mouseenter", function () {
+  const pItems = this.querySelectorAll("p");
+  pItems[0].innerText = "mouseenter";
+  pItems[1].innerText = ++e;
 });
--- 240806/script8.js
+++ 240806/script8.js
@@ -1,12 +1,24 @@
-document.querySelector("p").addEventListener("click", () => {
-  console.log("p");
+//mouseover & mouseout
+
+let = o = 0;
+
+const overOut = document.querySelector("div.overout");
+overOut.addEventListener("mouseover", function () {
+  console.log("hover");
 });
 
-document
-  .querySelector("section")
-  .addEventListener("click", console.log("section"));
+//mouseenter & mouseleave
+let e = 0;
 
-document.querySelector("div").addEventListener("click", console.log("div"));
+// document.querySelector("p").addEventListener("click", () => {
+//   console.log("p");
+// });
+
+// document
+//   .querySelector("section")
+//   .addEventListener("click", console.log("section"));
+
+// document.querySelector("div").addEventListener("click", console.log("div"));
 
 //웹 브라우저가 실행 =>html, js 파싱 (*문서읽으면서, 기본적으로 실행될 수 있는 요소 실행)
 
@@ -16,10 +28,10 @@ document.querySelector("div").addEventListener("click", console.log("div"));
 
 // testFnc();
 
-const elements = document.querySelectorAll("*");
+// const elements = document.querySelectorAll("*");
 
-elements.forEach(element) => {
-  element.addEventListener("click",(e) =>{
-    console.log(`e.target : ${e.target.tagName}, e.currentTarget : ${e.currentTarget.tagName}`)
-  })
-}
+// elements.forEach(element) => {
+//   element.addEventListener("click",(e) =>{
+//     console.log(`e.target : ${e.target.tagName}, e.currentTarget : ${e.currentTarget.tagName}`)
+//   })
+// }
