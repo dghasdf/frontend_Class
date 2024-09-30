44a04aba9e30b3a3048d3570c3921a5cdddc62ac
dghasdf <az3014@naver.com>
2024-09-30 21:32:01 +0900

    240923

--- 240923/sns-flatform/src/components/routers/Home.tsx
+++ 240923/sns-flatform/src/components/routers/Home.tsx
@@ -1,13 +1,19 @@
-import { useState } from "react";
 import styled from "styled-components";
 import PostForm from "../PostForm";
+import TimeLine from "../TimeLine";
 
-const Wrapp = styled.div``;
+const Wrapp = styled.div`
+  display: grid;
+  grid-template-rows: 1fr 5fr;
+  gap: 50px;
+  height: 90vh;
+`;
 
 const Home = () => {
   return (
     <Wrapp>
       <PostForm />
+      <TimeLine />
     </Wrapp>
   );
 };
