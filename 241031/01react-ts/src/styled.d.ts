a16f0946dddd3249461978c296aad83e92454afb
dghasdf <az3014@naver.com>
2024-10-31 20:13:08 +0900

    241031

---
+++ 241031/01react-ts/src/styled.d.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+declare module "stykled-components" {
+  export interface DefalutTheme {
+    bgColor: string;
+    textColor: string;
+    accentColor: string;
+    cardBgColor: string;
+  }
+}
